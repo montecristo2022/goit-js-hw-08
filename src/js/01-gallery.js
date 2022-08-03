dc4ad1abb121becc56fbf580ff42be06bd6bdc03
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2022-08-03 20:59:45 +0300

    bag

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
 
+
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const gallaryList = document.querySelector(".gallery")
+
+gallaryList.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems))
+
+new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: "alt" });
+
+
+
+
+
+
+
+
+
+function createGalleryMarkup  (galleryEl) {
+    return galleryEl.map(({preview, original, description}) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image"
+  src="${preview}"
+   data-source="${original}"
+  alt="${description}" />
+  </a>
+</li>`).join("") 
+  
+};
 
