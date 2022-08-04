9215a2baf2fd4ae57472b1319397b658901995e7
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2022-08-04 13:27:41 +0300

    finsh 3d task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -26,14 +26,14 @@ new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: "alt" });
 
 
 function createGalleryMarkup  (galleryEl) {
-    return galleryEl.map(({preview, original, description}) => `<li class="gallery__item">
+    return galleryEl.map(({preview, original, description}) => `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
   <img class="gallery__image"
   src="${preview}"
    data-source="${original}"
   alt="${description}" />
   </a>
-</li>`).join("") 
+</div>`).join("") 
   
 };
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
 
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea"),
+}
+
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-msg"
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+
+    console.log('отправка формы');
+    evt.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+
+
+function onTextareaInput(evt) {
+    const message = evt.target.value
+localStorage.setItem(STORAGE_KEY, message)
+}
+
+
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY)
+
+    if (savedMessage) {
+        console.log(savedMessage);
+        refs.textarea.value = savedMessage;
+    }
+
+    
+}
