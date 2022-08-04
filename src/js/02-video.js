a5459952da2ff3a194dce4231fd0f6093d5ed0b8
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2022-08-04 12:33:23 +0300

    task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+  import throttle from "lodash.throttle";
+const LS_KEY = currentSecondOfVideo
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+    localStorage.setItem(LS_KEY, data.seconds)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+
+
+player.setCurrentTime(localStorage.getItem(LS_KEY)).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
 
